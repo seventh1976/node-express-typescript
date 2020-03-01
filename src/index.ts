0200ef88afa365261aab429a30a4e3d913330a84
seventh1976 <seventh1976@gmail.com>
2020-03-01 21:19:17 +0800

    Added views

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import path from "path";
+
 const app = express();
 const port = 8080;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.render("index");
 });
 
 app.listen(port, () => {
